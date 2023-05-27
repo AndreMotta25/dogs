6f3554a28ad83f587e08b0713d589f57fd5c940f
AndreMotta25 <andremotta16041998@gmail.com>
2023-05-27 17:27:16 -0300

    ✨ feat: make routes

---
+++ src/@types/Photo.ts
@@ -0,0 +1,8 @@
+type Photo = {
+    src:string;
+    title:string;
+    acessos:number
+    id:string
+  }
+  
+export default Photo;  
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import {UserStorage} from './Context/UserContext'
+import Account from './Pages/Account';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Components/Photo';
+import UserProfile from './Components/UserProfile';
+import NotFound from './Pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <BrowserRouter>
+        <UserStorage>
+          <Header/>
+            <main className='AppBody'>
+              <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='login/*' element={<Login/>}/>
+                <Route path='conta/*' element={<ProtectedRoute><Account/></ProtectedRoute>}/>
+                <Route path='foto/:id' element={<Photo/>}/>
+                <Route path='perfil/:user' element={<UserProfile/>}/>
+                <Route path='*' element={<NotFound/>} />
+              </Routes>
+            </main>
+          <Footer/>
+        </UserStorage>
+      </BrowserRouter>
     </div>
   );
 }
