2e10c4fe6a5740180d46d9b96ee913c3fb3850f4
AndreMotta25 <andremotta16041998@gmail.com>
2023-05-27 17:31:46 -0300

    ✨ feat: reset password

--- src/Pages/Login/Components/LoginPasswordReset/index.tsx
+++ src/Pages/Login/Components/LoginPasswordReset/index.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { FormEvent, useEffect, useState } from 'react'
+import Input from '../../../../Components/Input';
+import useForm from '../../../../Hooks/useForm';
+import Button from '../../../../Components/Button';
+import { PASSWORD_RESET } from '../../../../api';
+import useFetch from '../../../../Hooks/useFetch';
+import Error from '../../../../Components/Helper/Error';
+import { useNavigate } from 'react-router-dom';
+import Head from '../../../../Components/Helper/Head/Head';
 
 const LoginPasswordReset = () => {
+  const [login, setLogin] = useState('');
+  const [key, setKey] = useState('')
+  const password = useForm();
+  const {data,error,loading,request} = useFetch()
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get('key');
+    const login = params.get('login')
+
+    if(key) setKey(key);
+    if(login) setLogin(login);
+
+  },[])
+
+  const handleSubmit = async (event:FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if(password.validate()) {
+      const {options,url} = PASSWORD_RESET({login: login, key:key,password:password.value});
+      const {json,response} = await request(url,options);
+      if(response?.ok) navigate('/login')
+    }
+
+  }
+
   return (
-    <div>LoginPasswordReset</div>
+    <section className='animeLeft'>
+      <Head title='Reseta a senha' description='Nome do site Dogs, com o feed de fotos'/>
+      <h1 className='title'>Resete a Senha</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label='Nova Senha' type='password' name='password' validation={password}/>
+        {loading ? <Button disabled>Resetando...</Button>:<Button>Resetar</Button> }
+      </form>
+      <Error error={error}/>
+    </section>
   )
 }
 
